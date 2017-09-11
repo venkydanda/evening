40aecf67ee8cda9f8dd304d9344bb214743a8765
venkydanda <31054184+venkydanda@users.noreply.github.com>
2017-09-10 22:45:06 -0400

    added empty line

--- src/main/java/evening/multiplication.java
+++ src/main/java/evening/multiplication.java
@@ -4,6 +4,7 @@ public class multiplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 int numberOne = Integer.parseInt(args[0]);
 int numberTwo = Integer.parseInt(args[1]);
 int result = multiplication (numberOne,numberTwo);
